631da9082f6e40eb161860bdac7e795bdf0d6a3b
Alexis Farinango <paulpulamarin141220@gmail.com>
2024-11-11 22:36:31 -0500

    Pendiente pantalla de token verificado deep link

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { enableScreens } from 'react-native-screens';
@@ -8,8 +8,9 @@ import RegistroEstudiante from "./screens/RegisterScreen";
 import RecuperarContra from "./screens/RecoverPassword";
 import StudentNavigation from "./RoutesStudent";
 import TeacherNavigation from "./RoutesTeacher";
+import TokenValidado from "./screens/TokenValidate";
 import { AuthProvider } from './context/AuthContext'; // Ajusta la ruta según tu estructura
-
+import { Linking } from 'react-native';
 
 enableScreens();
 const Stack = createNativeStackNavigator();
@@ -24,7 +25,37 @@ export default function App() {
 
 const MainApp = () => {
   const [initialRoute, setInitialRoute] = useState("Home");
+  
+  // useEffect(() => {
+  //   // Función para manejar el deep link cuando la aplicación se abre desde un enlace
+  //   const handleDeepLink = (url) => {
+  //     if (url && url.includes("/recuperar-password/")) {
+  //       const token = url.split("/recuperar-password/")[1]; // Extrae el token del URL
+  //       setInitialRoute("Token Validado");
+  //       navigationRef.current?.navigate("Token Validado", { token });
+  //     }
+  //   };
 
+  //   // Obtener el enlace inicial si la app se abre desde un deep link
+  //   const getInitialURL = async () => {
+  //     const initialUrl = await Linking.getInitialURL();
+  //     if (initialUrl) {
+  //       handleDeepLink(initialUrl);
+  //     }
+  //   };
+
+  //   // Escuchar cambios en el deep link mientras la app está abierta
+  //   const urlListener = Linking.addEventListener('url', (event) => {
+  //     handleDeepLink(event.url);
+  //   });
+
+  //   getInitialURL();
+
+  //   // Limpieza del listener al desmontar el componente
+  //   return () => {
+  //     urlListener.remove();
+  //   };
+  // }, []);
 
 
   return (
@@ -36,6 +67,7 @@ const MainApp = () => {
         <Stack.Screen name="Recuperar Contraseña" component={RecuperarContra} />
         <Stack.Screen name="Docente" component={TeacherNavigation} options={{headerShown: false}}/>
         <Stack.Screen name="Estudiante" component={StudentNavigation} options={{headerShown: false}}/>
+        <Stack.Screen name="Token Validado" component={TokenValidado} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ screens/TokenValidate.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function TokenValidado(){
+    return(
+        <View>
+            <Text>
+            Hola mundo
+            </Text>
+        </View>
+    )
+    
+}
