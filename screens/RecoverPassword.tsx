40ddcd066e2f2f16b5176d2bad994f7e6f127257
Alexis Farinango <paulpulamarin141220@gmail.com>
2024-12-07 23:37:54 -0500

    Ultimo con APK y validación codigo de errores

--- screens/RecoverPassword.tsx
+++ screens/RecoverPassword.tsx
@@ -1,47 +1,55 @@
 import React, { useState } from "react";
-import { View,Text, StyleSheet, TouchableOpacity,TextInput } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, TextInput } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import Toast from "react-native-toast-message";
 import axios from "axios";
 import { API_URL_BACKEND } from '@env'
 
 
-export default function RecuperarContra(){
-    const [email,setEmail]= useState("");
+export default function RecuperarContra() {
+    const [email, setEmail] = useState("");
     const navigation = useNavigation();
-    const handleRecuperar=async()=>{
-        if(!email){
+    const handleRecuperar = async () => {
+        if (!email) {
             Toast.show({
                 type: "error",
                 text1: "Por favor ingresa el correo",
             });
             return;
-        }else{
+        } else {
             try {
-                const response = await axios.post(`${API_URL_BACKEND}/estudiante/recuperar-password`,{
-                    email:email,
+                const response = await axios.post(`${API_URL_BACKEND}/estudiante/recuperar-password`, {
+                    email: email,
                 })
-                console.log("Email para recuperar constraseña",email);
-                
+                console.log("Email para recuperar constraseña", email);
+
                 Toast.show({
                     type: 'success',
                     text1: `${response.data.msg}`,
                     text2: 'Porfavor, revisa tu correo'
                 })
-                setTimeout(()=>{
-                        navigation.navigate('Token Validado')
-                },5000);
+                setTimeout(() => {
+                    navigation.navigate('Token Validado')
+                }, 5000);
             } catch (error) {
-                console.log("Se ha presentado el siguiente error",error);
-                Toast.show({
-                    type: 'error',
-                    text1: 'Correo electronico no encontrado',
-                    text2: 'Verifica el correo ingresado'
-                })
-                
+                console.log("Se ha presentado el siguiente error", error);
+                const status = error.response.status;
+                if (status === 404) {
+                    Toast.show({
+                        type: 'error',
+                        text1: `Estudiante no registrado`,
+                        text2: "Verifica el correo ingresado",
+                    })
+                } else {
+                    Toast.show({
+                        type: 'error',
+                        text1: `Problemas con el Servidor`,
+                        text2: `Revisa tu conexión a Internet`,
+                      })
+                }
             }
         }
-        
+
     }
     const styles = StyleSheet.create({
         container: {
@@ -86,16 +94,16 @@ export default function RecuperarContra(){
             fontWeight: "bold",
         },
     });
-    return(
+    return (
         <View style={styles.container}>
-            <Toast/>
+            <Toast />
             {/* <Text style={styles.title}>Recuperación de Contraseña</Text> */}
             <Text style={styles.label}>Ingresa tu correo Electrónico</Text>
-            <TextInput style={styles.input} placeholder="Correo institucional para recuperación" value={email} onChangeText={setEmail} keyboardType="email-address"/>
+            <TextInput style={styles.input} placeholder="Correo institucional para recuperación" value={email} onChangeText={setEmail} keyboardType="email-address" />
             <TouchableOpacity style={styles.button} onPress={handleRecuperar}>
                 <Text style={styles.buttonText}>Enviar</Text>
             </TouchableOpacity>
-            <Toast/>
+            <Toast />
         </View>
     )
 }
